39a4581dddaaeaa6f36e970233469862342db2b1
Andrey <Hitmanki@yandex.ru>
2022-11-09 20:12:42 +0400

    Initial Commit for Nest.js Project

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+	const PORT = process.env.PORT || 9001;
+	try {
+		await app.listen(PORT, () => 
+			console.log(`Running Money Transfer API on PORT ${PORT}`),
+		);
+	} catch (err){
+		console.log(err);
+	}
+}
+
+bootstrap();
