a8e84ea9e7e40142849261a23ddbf56ac9547943
Andrey <Hitmanki@yandex.ru>
2022-11-09 20:58:22 +0400

    create auth module and controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,5 @@
+import { Controller } from "@nestjs/common";
+import { Routes } from "src/utils/constants";
+
+@Controller(Routes.AUTH)
+export class AuthController {}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+
+@Module({
+	imports: [],
+	controllers: [AuthController],
+	providers: [],
+})
+
+export class AuthModule {}
