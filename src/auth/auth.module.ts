ed7950b5c663332ea86eebbd3bc303d6e0f93bb4
Andrey <Hitmanki@yandex.ru>
2022-12-09 10:29:08 +0400

    implements session serializer

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { UserModule } from "src/user/user.module";
 import { Services } from "src/utils/constants";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { SessionSeriaizer } from "./dtos/SesssionSerializer";
 import { LocalStrategy } from "./utils/LocalStrategy";
 
 @Module({
@@ -13,8 +14,8 @@ import { LocalStrategy } from "./utils/LocalStrategy";
 			provide: Services.AUTH,
 			useClass: AuthService,
 		},
+		SessionSeriaizer,
 		LocalStrategy,
 	],
 })
-
 export class AuthModule {}
